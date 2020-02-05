e37549640bda3bfe6d0175b43416672784c5b1d7
LRS4 <lrspencer@hotmail.co.uk>
2020-02-05 19:06:48 +0000

    add lxml to requirements

--- utils/scraper.py
+++ utils/scraper.py
@@ -54,11 +54,11 @@ def getDailyBriefing():
         newsHtml (str) : The HTML snippet parsed from the request
     """
     try:
-            r = requests.get("https://www.theweek.co.uk/dailybriefing", allow_redirects=True, verify=False)
-            source = r.text
-            soup = BeautifulSoup(source, 'lxml')
-            newsHtml = soup.findAll('div', {'class' : 'field-items'})
-            newsHtml = str(newsHtml[0]).replace('href="', 'href="https://www.theweek.co.uk')
+        r = requests.get("https://www.theweek.co.uk/dailybriefing", allow_redirects=True, verify=False)
+        source = r.text
+        soup = BeautifulSoup(source, 'lxml')
+        newsHtml = soup.findAll('div', {'class' : 'field-items'})
+        newsHtml = str(newsHtml[0]).replace('href="', 'href="https://www.theweek.co.uk')
     except:
         newsHtml = "<br/>Error :( News feed failed to load... try refreshing the page"
     
